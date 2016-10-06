c3cc2651e2d311de527dee6805105dfad5b7d070
Mohammed Shoukath <mohammed.shoukath@epsilon.com>
2016-10-05 22:33:06 -0400

    Fix | Fixed templateUrl logic.

--- src/app.tree-data.directive.js
+++ src/app.tree-data.directive.js
@@ -51,7 +51,7 @@ function ngCheckboxTreeGrid($timeout,
 
   function templateUrl(elem, attrs) {
     if (!attrs.templateUrl) {
-      return NgTreeTemplatesService[(attrs.gridType) ? "getTemplatePath" : "getTemplate"](attrs.gridType);
+      return NgCheckboxTree[(attrs.gridType) ? "getTemplatePath" : "getTemplate"](attrs.gridType);
     }
     return attrs.templateUrl;
   }
